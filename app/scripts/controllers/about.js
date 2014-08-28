0133ad45305581c3d09b170bf51f89129bcaa5b0
queenfrankie <laurawadden@gmail.com>
2014-08-28 17:28:40 +0200

    Adding sass and bower to project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kittymapgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kittymapgithubioApp
+ */
+angular.module('kittymapgithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kittymapgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kittymapgithubioApp
+ */
+angular.module('kittymapgithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
